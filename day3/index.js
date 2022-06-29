7ee5223c75813202c4d209ff64fd8d020e7ca0c4
santzforever <107780102+santzforever@users.noreply.github.com>
2022-06-29 01:56:21 -0700

    Add files via upload

---
+++ day3/index.js
@@ -0,0 +1,57 @@
+let numFloat = parseFloat(9.8)
+console.log(numFloat)
+let tech = 'python ,jargon'
+console.log(tech.includes('on'))
+let isMarried = false
+let year = 2022
+console.log(4 > 3)
+console.log (4>=3)
+let greater = 4>3
+console.log (greater)
+let greatequal = 4>=3
+console.log(greatequal)
+let lesser = 4<3
+console.log(lesser)
+let lesserequal = 4<=3
+console.log(lesserequal)
+let equalequal = 4==4
+console.log(equalequal)
+let notequal = 4!=4
+console.log(notequal)
+let notequalequal = 4!==4
+console.log(notequalequal)
+let notequalstring =4!='4'
+console.log(notequalstring)
+let stri = ('python')
+console.log(stri.length)
+let jar = ('jargon')
+console.log(jar.length)
+console.log("python.length", "jargon.length")
+let ampersand = 4>3 && 10<12
+console.log (ampersand)
+let ampersandfalse = 4>3&&10>12
+console.log(ampersandfalse)
+let negate =!(4>3)
+console.log(negate) 
+let negateb =!(4>3)
+console.log(negateb)
+let negatecom = !(4>3&&10<12)
+console.log(negatecom)
+const now = new Date()
+console.log(now.getFullYear())
+const nOw = new Date()
+console.log(nOw.getMonth())
+const nw = new Date()
+console.log (nw.getDate())
+const ne = new Date()
+console.log(ne.getTime())
+const area = 100
+let b = 20
+let h =10
+const areaoftriangle = 0.5 * b * h
+console.log(areaoftriangle)
+let sidea = 5
+let sideb = 4
+let sidec = 3
+const perimeterofthetriangle = sidea + sideb+ sidec
+console.log(perimeterofthetriangle)
